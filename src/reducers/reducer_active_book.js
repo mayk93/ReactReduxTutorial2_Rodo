10fa50e13d1afa162ae6a75674da1d7dd21b2769
mayk93 <mihai.mandrescu@gmail.com>
2016-05-16 01:42:44 +0100

    Book action implemented

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Michael on 16/05/16.
+ */
+
+// This state is not the application state. It's the state this reducer is responsible for.
+export default function (state = null, action) {
+    switch (action.type) {
+        case "BOOK_SELECTED":
+            return action.payload;
+        default:
+            return state;
+    }
+}
