72e541792c8d9aa57b36b3432ff02dc98f9e1137
mayk93 <mihai.mandrescu@gmail.com>
2016-05-16 01:53:04 +0100

    Book connection to detail view - Mapping done

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -3,8 +3,9 @@
  */
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
         return (
           <div>
@@ -13,3 +14,11 @@ export default class BookDetail extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+      book: state.activeBook;
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
